ee58a8dcec24807a57af3577516bc249bb0e4ef3
Akinleye stephen <sakinleye395@gmail.com>
2023-03-20 10:57:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -5,11 +6,8 @@
 *
 *Return: Always 1
 */
-int main (void)
+int main(void)
 {
-char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-write(1,msg,59);
-
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
